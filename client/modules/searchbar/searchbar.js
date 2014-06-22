9f38fc591488cdc68fffe4e2b84037e2f8335296
Matteo De Micheli <mat.demicheli@gmail.com>
2014-06-22 14:59:42 +0200

    Basic layout + First commit

--- client/modules/searchbar/searchbar.js
+++ client/modules/searchbar/searchbar.js
@@ -1,19 +1,6 @@
 Template.searchbar.helpers({
-  'foundSite' : function () {
-    var i,
-      foundRoutes = [],
-      allRoutes = Router.routes,
-      searchString = Session.get('searchforSites');
-
-    if (searchString && searchString.length > 0) {
-      for (i = 0; i < allRoutes.length; i += 1) {
-        if (allRoutes[i].name.indexOf(searchString) > -1) {
-          foundRoutes.push(allRoutes[i]);
-        }
-      }
-    }
-
-    return foundRoutes;
+  'interaction' : function () {
+    return 'search';
   }
 });
 
